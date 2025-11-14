4447840894c46fc4b20ec5e86c8ab1390d55b6e2
sahnoun122 <khadijasahnoun70@gmail.com>
2025-11-14 11:25:05 +0100

    feat: ajouter la forms de la login pour darna

---
+++ src/components/FormLoginDarna.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function FormLoginDarna({onSubmit}){
+    const [data , setData] = useState({
+        email: "",
+        password : "",
+    });
+
+    const change = (e)=>
+        setData({ ...data , [e.target.name]: e.target.value});
+
+    const submit = (e)=>{
+        e.preventDefault();
+        onsubmit(data);
+    }
+
+    return (
+      <form onSubmit={submit} className="space-y-3 border p-4 rounded-xl">
+        <h2 className="text-lg font-semibold text-center">Connexion Darna</h2>
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={change}
+          className="w-full border p-2 rounded"
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Mot de passe"
+          onChange={change}
+          className="w-full border p-2 rounded"
+        />
+
+        <button className="w-full bg-blue-600 text-white p-2 rounded">
+          Se connecter
+        </button>
+      </form>
+    );
+}
